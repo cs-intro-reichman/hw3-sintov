8be2052af1f34856e457336c203faa3cbfd76fa7
Or Sintov <sintov@Ors-Laptop.local>
2024-11-28 15:43:57 +0200

    anagram push

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,67 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		str1 = noSpaces(str1);
+		str2 = noSpaces(str2);
+		int substringPlace = 0;
+		if (str1.length() != str2.length()){
+			return false;
+		}
+		for (int i = 0; i < str1.length(); i ++){
+			boolean isAnagram = false;
+			for (int j = 0; j < str2.length(); j ++){
+				if(str1.charAt(i) == str2.charAt(j)){
+					isAnagram = true;
+					substringPlace = j;
+				}
+			}
+			if(isAnagram == false){
+				return false;
+			}
+			str2 = str2.substring(0, substringPlace) + str2.substring(substringPlace + 1);
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String afterProcessstr = "";
+		str = str.toLowerCase();
+		for (int i =0; i< str.length(); i++){
+			if(str.charAt(i) >= 'a' && str.charAt(i) <= 'z' || str.charAt(i) == 32){
+				afterProcessstr += str.charAt(i);
+			}
+		}
+		return afterProcessstr;
 	} 
+
+	public static String noSpaces (String str){
+		String noSpacesString = "";
+		for (int i = 0; i < str.length(); i++){
+			if (str.charAt(i) != 32){
+				noSpacesString += str.charAt(i);
+			}
+		}
+		return noSpacesString;
+	}
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
+		String anagramString = "";
+		String strToProcess = str;
+		//str = preProcess(str);
+		//str = noSpaces(str);
+		for (int i = 0; i < str.length(); i++){
+			int rnum = (int)((strToProcess.length()) * Math.random());
+			anagramString += strToProcess.charAt(rnum);
+			strToProcess = strToProcess.substring(0, rnum) + strToProcess.substring(rnum + 1);
+		}
 		// Replace the following statement with your code
-		return "";
+		return anagramString;
 	}
 }
